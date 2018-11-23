6b0c71f3b123e70f56efa94f02e34f1944bbf07b
Dawid Parafiński <vinniczek@gmail.com>
2018-11-23 09:31:36 +0100

    EZP-29713: Set tags to position before admin (#110)

---
+++ src/AppBundle/Menu/ReorderMenuListener.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace AppBundle\Menu;
+
+use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
+use Knp\Menu\Util\MenuManipulator;
+
+class ReorderMenuListener
+{
+    /**
+     * @param \EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent $event
+     */
+    public function moveTagsToLast(ConfigureMenuEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $manipulator = new MenuManipulator();
+        $manipulator->moveToLastPosition($menu['eztags']);
+    }
+}
